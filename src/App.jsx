aa54a615ff3c0a78621a8ef9033915480d785e69
Pushkar141 <pushkarshinde3232@gmail.com>
2025-01-10 22:32:27 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <h1 className='text-white text-6xl font-mono flex justify-center'>Chat</h1>
+    </>
   )
 }
 
